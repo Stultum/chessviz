8c3dc39d749bf26d7d656424c4b2dde5ce05c7d7
Stultum <tgslaayer@gmail.com>
2019-04-22 23:13:36 +0700

    Fixed some errors

--- src/board.h
+++ src/board.h
@@ -1,5 +1,9 @@
+#include <fstream>
+#include <string>
 #ifndef BOARD_PRINT_PLAIN_H
 #define BOARD_PRINT_PLAIN_H
+
+using namespace std;
 
 bool Do2Steps(char (*board)[9], string s);
 
--- src/board_print_plain.h
+++ src/board_print_plain.h
@@ -1,6 +1,6 @@
 #ifndef BOARD_PRINT_PLAIN_H
 #define BOARD_PRINT_PLAIN_H
 
-void PrintBoard(char (*chessB)[9]);
+void PrintBoard(char (*board)[9]);
 
 #endif
