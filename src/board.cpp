2d5b93cce7751a7d6a7de21a75f3fd561c210193
Stultum <tgslaayer@gmail.com>
2019-05-05 16:54:01 +0700

    Added possibility of moving other figs

--- src/board.cpp
+++ src/board.cpp
@@ -299,7 +299,323 @@ int Do2StepsPawns(char (*board)[9], char* s)
     return 1;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 int Do2StepsOther(char (*board)[9], char* s)
 {
+    char figure;
+    int x, y, x1, y1, i;
+    char step = s[4];
+    step = s[4];
+    figure = s[3];
+    cout << "'" << figure << "'" << endl;
+    if (figure != 'R' && figure != 'B' && figure != 'N' && figure != 'Q'
+        && figure != 'K') {
+        cout << "WRONG INPUTfig1" << endl;
+        return 0;
+    }
+    if (step == 'a')
+        x = 1;
+    else if (step == 'b')
+        x = 2;
+    else if (step == 'c')
+        x = 3;
+    else if (step == 'd')
+        x = 4;
+    else if (step == 'e')
+        x = 5;
+    else if (step == 'f')
+        x = 6;
+    else if (step == 'g')
+        x = 7;
+    else if (step == 'h')
+        x = 8;
+    else {
+        cout << "WRONG INPUT1" << endl;
+        return 0;
+    }
+    step = s[5];
+    if (step == '1')
+        y = 7;
+    else if (step == '2')
+        y = 6;
+    else if (step == '3')
+        y = 5;
+    else if (step == '4')
+        y = 4;
+    else if (step == '5')
+        y = 3;
+    else if (step == '6')
+        y = 2;
+    else if (step == '7')
+        y = 1;
+    else if (step == '8')
+        y = 0;
+    else {
+        cout << "WRONG INPUT2" << endl;
+        return 0;
+    }
+
+    step = s[8];
+    if (step == '1')
+        y1 = 7;
+    else if (step == '2')
+        y1 = 6;
+    else if (step == '3')
+        y1 = 5;
+    else if (step == '4')
+        y1 = 4;
+    else if (step == '5')
+        y1 = 3;
+    else if (step == '6')
+        y1 = 2;
+    else if (step == '7')
+        y1 = 1;
+    else if (step == '8')
+        y1 = 0;
+    else {
+        cout << "WRONG INPUT3" << endl;
+        return 0;
+    }
+
+    step = s[7];
+    if (step == 'a')
+        x1 = 1;
+    else if (step == 'b')
+        x1 = 2;
+    else if (step == 'c')
+        x1 = 3;
+    else if (step == 'd')
+        x1 = 4;
+    else if (step == 'e')
+        x1 = 5;
+    else if (step == 'f')
+        x1 = 6;
+    else if (step == 'g')
+        x1 = 7;
+    else if (step == 'h')
+        x1 = 8;
+    else {
+        cout << "WRONG INPUT4" << endl;
+        return 0;
+    }
+
+    cout << endl;
+
+    for (i = 0; i <= 8; i++) {
+        cout << s[i];
+    }
+    cout << endl << endl;
+
+    if (board[y][x] != figure) {
+        cout << "WRONG INPUT" << endl;
+        return 0;
+    }
+
+    if (board[y][x] == ' ') {
+        cout << "WRONG INPUT5" << endl;
+        return 0;
+    }
+
+    //   if (s[6] != 'x' && x1 != x) {
+    //     cout << "WRONG INPUT6" << endl;
+    //   return 0;
+    //}
+
+    if (s[6] == 'x' && x1 == x) {
+        cout << "WRONG INPUT7" << endl;
+        return 0;
+    }
+
+    if (y1 >= y) {
+        cout << "WRONG INPUT8" << endl;
+        return 0;
+    }
+
+    if (s[6] == 'x') {
+        if (board[y1][x1] != ' ') {
+            board[y1][x1] = board[y][x];
+            board[y][x] = ' ';
+        } else {
+            cout << "WRONG INPUT9" << endl;
+            return 0;
+        }
+    } else if (s[6] == '-') {
+        if (board[y1][x1] == ' ') {
+            board[y1][x1] = board[y][x];
+            board[y][x] = ' ';
+        } else {
+            cout << "WRONG INPUT10" << endl;
+            return 0;
+        }
+    } else {
+        cout << "WRONG INPUT11" << endl;
+        return 0;
+    }
+
+    PrintBoard(board);
+
+    ////////////////////////////
+
+    step = s[11];
+    figure = s[10];
+    if (figure == 'B')
+        figure = 'b';
+    if (figure == 'R')
+        figure = 'r';
+    if (figure == 'N')
+        figure = 'n';
+    if (figure == 'Q')
+        figure = 'q';
+    if (figure == 'K')
+        figure = 'k';
+    cout << "'" << figure << "'" << endl;
+    if (figure != 'r' && figure != 'b' && figure != 'n' && figure != 'q'
+        && figure != 'k') {
+        cout << "WRONG INPUT1" << endl;
+        return 0;
+    }
+    if (step == 'a')
+        x = 1;
+    else if (step == 'b')
+        x = 2;
+    else if (step == 'c')
+        x = 3;
+    else if (step == 'd')
+        x = 4;
+    else if (step == 'e')
+        x = 5;
+    else if (step == 'f')
+        x = 6;
+    else if (step == 'g')
+        x = 7;
+    else if (step == 'h')
+        x = 8;
+    else {
+        cout << "WRONG INPUT1" << endl;
+        return 0;
+    }
+
+    step = s[12];
+    if (step == '1')
+        y = 7;
+    else if (step == '2')
+        y = 6;
+    else if (step == '3')
+        y = 5;
+    else if (step == '4')
+        y = 4;
+    else if (step == '5')
+        y = 3;
+    else if (step == '6')
+        y = 2;
+    else if (step == '7')
+        y = 1;
+    else if (step == '8')
+        y = 0;
+    else {
+        cout << "WRONG INPUT2" << endl;
+        return 0;
+    }
+
+    step = s[15];
+    if (step == '1')
+        y1 = 7;
+    else if (step == '2')
+        y1 = 6;
+    else if (step == '3')
+        y1 = 5;
+    else if (step == '4')
+        y1 = 4;
+    else if (step == '5')
+        y1 = 3;
+    else if (step == '6')
+        y1 = 2;
+    else if (step == '7')
+        y1 = 1;
+    else if (step == '8')
+        y1 = 0;
+    else {
+        cout << "WRONG INPUT3" << endl;
+        return 0;
+    }
+
+    step = s[14];
+    if (step == 'a')
+        x1 = 1;
+    else if (step == 'b')
+        x1 = 2;
+    else if (step == 'c')
+        x1 = 3;
+    else if (step == 'd')
+        x1 = 4;
+    else if (step == 'e')
+        x1 = 5;
+    else if (step == 'f')
+        x1 = 6;
+    else if (step == 'g')
+        x1 = 7;
+    else if (step == 'h')
+        x1 = 8;
+    else {
+        cout << "WRONG INPUT4" << endl;
+        return 0;
+    }
+
+    cout << endl;
+
+    cout << s[0] << s[1] << ' ';
+
+    for (i = 10; i <= 15; i++) {
+        cout << s[i];
+    }
+    cout << endl << endl;
+
+    if (board[y][x] == ' ') {
+        cout << "WRONG INPUT5" << endl;
+        return 0;
+    }
+
+    if (board[y][x] != figure) {
+        cout << "WRONG INPUT6" << endl;
+        return 0;
+    }
+
+    //  if (s[13] != 'x' && x1 != x) {
+    //     cout << "WRONG INPUT7" << endl;
+    //    return 0;
+    //}
+    if (s[13] == 'x' && x1 == x) {
+        cout << "WRONG INPUT8" << endl;
+        return 0;
+    }
+
+    if (y1 <= y) {
+        cout << "WRONG INPUT9" << endl;
+        return 0;
+    }
+
+    if (s[13] == 'x') {
+        if (board[y1][x1] != ' ') {
+            board[y1][x1] = board[y][x];
+            board[y][x] = ' ';
+        } else {
+            cout << "WRONG INPUT10" << endl;
+            return 0;
+        }
+    } else if (s[13] == '-') {
+        if (board[y1][x1] == ' ') {
+            board[y1][x1] = board[y][x];
+            board[y][x] = ' ';
+        } else {
+            cout << "WRONG INPUT11" << endl;
+            return 0;
+        }
+    } else {
+        cout << "WRONG INPUT12" << endl;
+        return 0;
+    }
+
+    PrintBoard(board);
     return 1;
 }
